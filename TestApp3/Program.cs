4ace471b32e0809be7846a778829e51d0c266176
YuriyFedosov <92025361+YuriyFedosov@users.noreply.github.com>
2021-10-14 00:38:25 +0300

    3

--- TestApp3/Program.cs
+++ TestApp3/Program.cs
@@ -19,7 +19,33 @@ namespace TestApp3
             квадратов со стороной C (без наложений). Найти количество квадратов, размещенных на прямоугольнике. 
             Операции умножения и деления не использовать.*/
 
-
+            int valueA, valueB, valueC, valueX, valueY, valueS;
+            Console.WriteLine("A");
+            valueA= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("B");
+            valueB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("C");
+            valueC = Convert.ToInt32(Console.ReadLine());
+            
+            valueX = valueC;
+            valueY = valueC;
+            valueS = 0;
+            
+            while (valueX<=valueA)
+            {
+                valueS++;
+                valueX = valueX + valueC;
+            }
+            valueX = valueS;
+            valueS = 0;
+            while (valueY<=valueB)
+            {
+                valueS = valueS + valueX;
+                valueY = valueY + valueC;
+                               
+            }
+            Console.WriteLine("--->{0}", valueS);
+            Console.ReadKey();
         }
     }
 }
